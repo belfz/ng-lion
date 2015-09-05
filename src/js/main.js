181ae3f8821c11a3830649c921bbe4f1d1c0dca8
Marcin Baraniecki <marcin.baraniecki@mttnow.com>
2015-09-05 16:59:13 +0200

    cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,5 @@ require('angular');
 require('./lion/lion.module.js');
 
 angular.element(document).ready(function () {
-	var t = angular.module('lion');
 	angular.bootstrap(document, ['lion']);
 });
-
-module.exports = angular.module('lion');
